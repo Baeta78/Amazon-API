32cba951f268d35006edf6c288445f315cf66bee
JONAS BAETA ALVES ROSA <93152222+Baeta78@users.noreply.github.com>
2024-09-12 19:55:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 import datetime
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+import json
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -31,21 +32,27 @@ def create_signed_headers(payload, host, region, service):
     amz_date = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
     date_stamp = datetime.datetime.utcnow().strftime('%Y%m%d')
 
+    # Payload convertido para JSON e gerado o hash SHA-256
+    payload_json = json.dumps(payload)
+    payload_hash = hashlib.sha256(payload_json.encode('utf-8')).hexdigest()
+
     canonical_headers = f'host:{host}\n'
     signed_headers = 'host'
 
-    payload_hash = hashlib.sha256(payload.encode('utf-8')).hexdigest()
-
+    # Criação do canonical request
     canonical_request = f'POST\n/paapi5/getitems\n\n{canonical_headers}\n{signed_headers}\n{payload_hash}'
     credential_scope = f'{date_stamp}/{region}/{service}/aws4_request'
-    
+
+    # String para assinar
     string_to_sign = f'AWS4-HMAC-SHA256\n{amz_date}\n{credential_scope}\n{hashlib.sha256(canonical_request.encode("utf-8")).hexdigest()}'
     signing_key = get_signature_key(SECRET_ACCESS_KEY, date_stamp, region, service)
-    
+
+    # Geração da assinatura
     signature = hmac.new(signing_key, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
 
+    # Cabeçalhos de autorização
     authorization_header = f'AWS4-HMAC-SHA256 Credential={ACCESS_KEY_ID}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}'
-    
+
     headers = {
         'Content-Type': 'application/json; charset=utf-8',
         'X-Amz-Date': amz_date,
@@ -59,7 +66,7 @@ def create_signed_headers(payload, host, region, service):
 def webhook():
     data = request.json
     item_ids = data.get('itemIds')
-    
+
     if not item_ids:
         return jsonify({'error': 'itemIds inválido ou não fornecido'}), 400
 
@@ -74,11 +81,14 @@ def webhook():
     host = 'webservices.amazon.com.br'
     region = 'us-east-1'
     service = 'ProductAdvertisingAPI'
-    
-    headers = create_signed_headers(payload=str(payload), host=host, region=region, service=service)
 
+    # Gerar cabeçalhos assinados
+    headers = create_signed_headers(payload=payload, host=host, region=region, service=service)
+
+    # Fazer a requisição
     response = requests.post(f'https://{host}/paapi5/getitems', json=payload, headers=headers)
-    
+
+    # Verificar a resposta
     if response.status_code == 200:
         return jsonify(response.json())
     else:
